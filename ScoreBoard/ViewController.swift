6b62cbcd81834975a8bc65cd9afd00c6573c8819
Lucas <wei04161995lu@gmail.com>
2023-09-02 22:58:02 +0800

    Initial Commit

---
+++ ScoreBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreBoard
+//
+//  Created by chiaowei on 2023/9/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
